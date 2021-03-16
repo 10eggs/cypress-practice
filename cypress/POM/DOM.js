5a34a75f456b4edc302a15ecda66bd2d57b86317
r2williams <rebecca.williams@historicengland.org.uk>
2021-03-16 17:21:52 +0000

    iframes tests

---
+++ cypress/POM/DOM.js
@@ -0,0 +1,14 @@
+const webElems ={
+    usernameInput : '//input[@id="username"]',
+    passwordInput : '//input[@id="password"]',
+    signInBtn: '//button[@type="submit"]',
+    loginSignInBtn : '//a[@class="application-login"]',
+    expandNavBarBtn : '//button[@class="navbar-toggle"]',
+    welcomeBtn: '//a[@href="/user"]',
+    manageDataBtn: '//a[@href="/resource"]',
+    createHamResource : '//*[@id="resource-list"]/div[6]/div[2]/div/a',
+    resourceEditorTree : '.resource-editor-tree',
+    ddlSelectedOption : '.select2-chosen'
+};
+
+export {webElems}
