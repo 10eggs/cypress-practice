3e08e5f4a3aeb6730094924fdeb3317f1600dbb4
r2williams <rebecca.williams@historicengland.org.uk>
2021-03-22 19:38:16 +0000

    practice methods

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -46,3 +46,41 @@ describe('Search functionality', function(){
 
 })
 })
+
+// describe('Authentication functionality', () => {
+//     beforeEach(() => {
+//         Cypress.Cookies.preserveOnce('ai_session', 'csrftoken','warden','ai_user'); 
+//         cy.visit('/');   
+// })
+
+
+//     it('User can login using valid credentials', () => {
+//         cy.log('********** sign into the application **********');
+//         cy.fixture('test-data').then(function (data) {
+//             this.data = data;
+//         cy.xpath(homePage.navBar).click();
+//         cy.xpath(homePage.signInBtn).click();
+//         cy.xpath(loginPage.username).type(this.data.Username);
+//         cy.xpath(loginPage.password).type(this.data.Password);
+//         cy.xpath(loginPage.signInBtn).click();
+//         cy.xpath(homePage.navBar).click();
+
+//         cy.log('********** verify user is logged in **********')
+//         cy.xpath(homePage.logOffBtn).contains('Log off').should('be.visible');
+//         cy.xpath(homePage.logOffBtn).click();
+//     })
+
+//   })
+
+//     it('User cannot login using invalid credentials', () =>{
+//         cy.xpath(homePage.navBar).click();
+//         cy.xpath(homePage.signInBtn).click();
+//         cy.xpath(loginPage.username).type('test');
+//         cy.xpath(loginPage.password).type('test');
+//         cy.xpath(loginPage.signInBtn).click();
+
+//         cy.log('********** verify error message is visible **********')
+//         cy.get('login-failed-alert').should('be.visible');
+//     })
+
+// })
