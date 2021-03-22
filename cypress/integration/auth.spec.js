2872d83b973e9ed9c9c09ec560f02f28c05ddc9d
r2williams <rebecca.williams@historicengland.org.uk>
2021-03-22 19:37:04 +0000

    authetification test update

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -1,41 +1,36 @@
 /// <reference types="cypress" />
-import { webElements as homePage } from '../POM/home_page'
-import { webElements as loginPage } from '../POM/login_page'
 
-describe('Authentication functionality', () => {
+describe('Login functionality', () => {
+
     beforeEach(() => {
-        Cypress.Cookies.preserveOnce('ai_session', 'csrftoken','warden','ai_user'); 
-        cy.visit('/');   
-})
-
-
-    it('User can login using valid credentials', () => {
-        cy.log('********** sign into the application **********');
+        cy.visit('/');
         cy.fixture('test-data').then(function (data) {
             this.data = data;
-        cy.xpath(homePage.navBar).click();
-        cy.xpath(homePage.signInBtn).click();
-        cy.xpath(loginPage.username).type(this.data.Username);
-        cy.xpath(loginPage.password).type(this.data.Password);
-        cy.xpath(loginPage.signInBtn).click();
-        cy.xpath(homePage.navBar).click();
-
-        cy.log('********** verify user is logged in **********')
-        cy.xpath(homePage.logOffBtn).contains('Log off').should('be.visible');
-        cy.xpath(homePage.logOffBtn).click();
+        })
     })
 
-  })
+    it('navigates to the sign in page', () => {
+        cy.xpath('//button[@class="navbar-toggle"]').click({ force: true });
+        cy.xpath('//*[@id="auth-link"]').click();
+        cy.url().should('eq', 'https://stage-warden.historicengland.org.uk/auth/?next=/index.htm');
+    })
 
-    it('User cannot login using invalid credentials', () =>{
-        cy.xpath(homePage.navBar).click();
-        cy.xpath(homePage.signInBtn).click();
-        cy.xpath(loginPage.username).type('test');
-        cy.xpath(loginPage.password).type('test');
-        cy.xpath(loginPage.signInBtn).click();
+    it('displays an error message when logging in with invalid credentials', () => {
+        cy.get('input[name=username]').type('test');
+        cy.get('input[name=password]').type('test');
+        cy.get('button[type=submit]').click();
 
-        cy.log('********** verify error message is visible **********')
-        cy.get('login-failed-alert').should('be.visible');
+        cy.get('login-failed-alert').should('be.visible').and('contain', 'Login failed');
+    })
+
+    it('can login using valid credentials', () => {
+        cy.get('input[name=username]').clear().type(this.data.Username);
+        cy.get('input[name=password]').clear().type(this.data.Password);
+        cy.get('button[type=submit]').click();
+
+        cy.get('[data-target=#myNavbar]').click();
+        cy.get('.application-login').should('contain', 'Log off');
+        cy.getCookie('cypress-session-cookie').should('exist');
     })
 
 })
