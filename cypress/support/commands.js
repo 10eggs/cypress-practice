75872130d17a4f5f99ee7ebdfdc4f82388858c4f
r2williams <rebecca.williams@historicengland.org.uk>
2021-04-07 16:56:25 +0100

    Added custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,11 @@
 Cypress.Commands.add('selectCard', (card) => {
-    cy.get('span').contains(card).click()
+    cy.get('span').contains(card).click();
+})
+
+Cypress.Commands.add('selectDropDownOption', (dropDownOption) => {
+    cy.get('select2-results').contains(dropDownOption).click();
+})
+
+Cypress.Commands.add('clickButton', (buttonLabel) => {
+    cy.get('button').contains(buttonLabel).click();
 })
