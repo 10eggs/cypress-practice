5c8090f8f3a48390515e6133301ed0818492b533
r2williams <rebecca.williams@historicengland.org.uk>
2021-04-07 16:55:22 +0100

    updated advanced search tests

--- cypress/integration/advancedSearch.spec.js
+++ cypress/integration/advancedSearch.spec.js
@@ -22,29 +22,39 @@ describe('advanced search functionality', () => {
         cy.xpath(loginPage.signInBtn).click();
         cy.xpath(homePage.navBar).click();
         cy.xpath(homePage.logOffBtn).contains('Log off').should('be.visible');
-
-        cy.log('********** navigate to search page **********')
-        cy.xpath(homePage.navBar).click();
-        cy.xpath(homePage.searchBtn).click();
-        cy.get('#s2id_autogen2').should('be.visible');
     })
 })
 
     /*************************************************************************************/
 
-    it('@1 should be able to search for a Monument with primary name type like test monument', () => {
+    it('should navigate to the search page', () => {
+        cy.log('********** navigate to search page **********')
+        cy.xpath(homePage.navBar).click();
+        cy.xpath(homePage.searchBtn).click();
+        cy.get('.select2-input').should('be.visible');
+    })
+
+     /*************************************************************************************/
+
+    it('@1 should be able to search for a Monument with primary name type', () => {
         cy.log('********** select only monument resources **********')
         cy.xpath(searchPage.rsrcType).click();
         cy.xpath(searchPage.monRsrc).click();
 
+        cy.log('********** enter asset name into advanced search facet **********')
         cy.xpath(searchPage.advSearchBtn).click();
         cy.xpath(searchPage.monAssetName).click( { force: true });
         cy.xpath(searchPage.monNameField).type('test monument');
-        cy.xpath(searchPage.dropDown).click();
-        cy.xpath(searchPage.nameEq).click();
+
+        cy.log('********** select the asset name type **********')
+        cy.xpath(searchPage.assetNameDropDown).click();
+
+        // Can't work out why it doesn't like any of the selectors for the asset name dropdown :(
+        cy.get('#s2id_autogen312_search').type('Primary{enter}');
+        cy.get('.select2-chosen').should('have.value', 'Primary');
 
         cy.log('********** verify the search result amount has decreased **********');
-        cy.get('.search-title').contains('Results: 572');
+        cy.get('.search-listing-title').contains('1358845');
     })
 
     // it('@2 should be able to search for a monument using latitude and longitude coordinates', () => {
